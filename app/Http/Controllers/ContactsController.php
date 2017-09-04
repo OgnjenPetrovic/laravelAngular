d687a834fb303f31672ddbffdaa27164781aa7d8
OgnjenPetrovic <zoomght@gmail.com>
2017-09-04 20:48:45 +0200

    PRVI KOMIT BAZA FUNKCIONALNA

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Contact;
 
 class ContactsController extends Controller
 {
@@ -13,7 +14,7 @@ class ContactsController extends Controller
      */
     public function index()
     {
-        return 'fasdfsdfdsf';
+        return Contact::all();
     }
 
     /**
